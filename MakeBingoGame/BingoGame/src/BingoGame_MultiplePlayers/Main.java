ab05c9f5d27c51dc8c99af5eca6c8ca479916d87
OkabeYuma <okabeyuma@OkabeYuma-no-MacBook-Air.local>
2018-04-16 17:21:34 +0900

    Correct Main class method

--- MakeBingoGame/BingoGame/src/BingoGame_MultiplePlayers/Main.java
+++ MakeBingoGame/BingoGame/src/BingoGame_MultiplePlayers/Main.java
@@ -8,26 +8,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
 		System.out.print("ビンゴカードのサイズを決めてください：");
-		int sideSize;
-		while (true) {
-			sideSize = scanner.nextInt();
-			if (sideSize > 0 && sideSize <= 10) {
-				break;
-			}
-			System.out.print("カードのサイズは1〜10の範囲しか選べません！：");
-		}
-		System.out.println(sideSize + "×" + sideSize + "マスのビンゴを始めます");
+		int sideSize = scanner.nextInt();
+		setSideSize(sideSize);
 		System.out.print("何人でプレイしますか？：");
-		int playerNumber;
-		while (true) {
-			playerNumber = scanner.nextInt();
-			if (playerNumber > 0) {
-				break;
-			}
-			System.out.print("プレイヤーは1人以上です！：");
-		}
+		int playerNumber = scanner.nextInt();
+		setPlayerNumber(playerNumber);
 
 		/* プレイヤー人数分のcardオブジェクトを生成 */
 		List<BingoCard> card = new ArrayList<>();
@@ -47,30 +33,59 @@ public class Main {
 			if (scanner.nextInt() == 0) {
 				break;
 			}
-			int lotteryNumber = lot.lottery();
-			lotCount++;
-			System.out.println();
-			System.out.println(lotCount + "回目：当選番号は" + lotteryNumber + "です");
-
-			/* プレイヤー毎にビンゴしたかどうか判定する */
-			int bingoPlayerCount = 0;
-			for (int i = 0; i < playerNumber; i++) {
-				System.out.println("");
-				System.out.println("【Player" + (i + 1) + "】");
-				card.get(i).judgeHit(lotteryNumber);
-				card.get(i).showTable();
-				if (card.get(i).judgeBingo()) {
-					System.out.println(card.get(i).getBingoCount() + "ビンゴ！！！");
-					bingoPlayerCount++;
-				}
-			}
-
-			/* ビンゴした人がいる場合は人数を表示して終了 */
+			int lotteryNumber = getLotteryNumber(lot, lotCount);
+			int bingoPlayerCount = checkBingo(card, playerNumber, lotteryNumber);
 			if (bingoPlayerCount > 0) {
 				System.out.println("");
 				System.out.println(bingoPlayerCount + "人ビンゴしたのでおわり");
 				break;
 			}
 		}
+	}
+
+	// ビンゴカードのサイズが適切か確認するメソッド
+	public static void setSideSize(int sideSize) {
+		while (true) {
+			if (sideSize > 0 && sideSize <= 10) {
+				break;
+			}
+			System.out.print("カードのサイズは1〜10の範囲しか選べません！：");
+		}
+		System.out.println(sideSize + "×" + sideSize + "マスのビンゴを始めます");
+	}
+
+	// プレイ人数が適切か確認するメソッド
+	public static void setPlayerNumber(int playerNumber) {
+		while (true) {
+			if (playerNumber > 0) {
+				break;
+			}
+			System.out.print("プレイヤーは1人以上です！：");
+		}
+	}
+
+	// 当選番号を取得・表示するメソッド
+	public static int getLotteryNumber(Lottery lot, int lotCount) {
+		int lotteryNumber = lot.lottery();
+		lotCount++;
+		System.out.println();
+		System.out.println(lotCount + "回目：当選番号は" + lotteryNumber + "です");
+		return lotteryNumber;
+	}
+
+	// ビンゴした人数を返すメソッド
+	public static int checkBingo(List<BingoCard> card, int playerNumber, int lotteryNumber) {
+		int bingoPlayerCount = 0;
+		for (int i = 0; i < playerNumber; i++) {
+			System.out.println("");
+			System.out.println("【Player" + (i + 1) + "】");
+			card.get(i).judgeHit(lotteryNumber);
+			card.get(i).showTable();
+			if (card.get(i).judgeBingo()) {
+				System.out.println(card.get(i).getBingoCount() + "ビンゴ！！！");
+				bingoPlayerCount++;
+			}
+		}
+		return bingoPlayerCount;
 	}
 }
