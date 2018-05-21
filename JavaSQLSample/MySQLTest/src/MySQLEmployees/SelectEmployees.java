da8dbb17952b2fc1660fc67f3acc4d2eb22b2819
OkabeYuma <okabeyuma@OkabeYuma-no-MacBook-Air.local>
2018-05-21 11:29:41 +0900

    Modify Java SQL sample ver2

--- JavaSQLSample/MySQLTest/src/MySQLEmployees/SelectEmployees.java
+++ JavaSQLSample/MySQLTest/src/MySQLEmployees/SelectEmployees.java
@@ -24,10 +24,10 @@ public class SelectEmployees {
 			Connection conn = DriverManager.getConnection(url, user, password);
 			Statement stmt = conn.createStatement();
 
-			insertDB(stmt, tableName);
-			deleteDB(stmt, tableName);
-			updateDB(stmt, tableName);
-			ResultSet rset = createSelectSQL(stmt, tableName);
+			insertEmployee(stmt, tableName);
+			deleteEmployee(stmt, tableName);
+			updateEmployee(stmt, tableName);
+			ResultSet rset = executeSelectSQL(stmt, tableName);
 			while (rset.next()) {
 				System.out.println(
 						rset.getInt(1) + "\t" + rset.getString(2) + "\t" + rset.getInt(3) + "\t" + rset.getString(4));
@@ -41,7 +41,7 @@ public class SelectEmployees {
 	}
 
 	// 社員情報を追加するメソッド
-	public static void insertDB(Statement stmt, String tableName) throws SQLException {
+	public static void insertEmployee(Statement stmt, String tableName) throws SQLException {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("社員情報を追加しますか？（1:追加する）：");
 		if (scanner.nextInt() == 1) {
@@ -54,35 +54,35 @@ public class SelectEmployees {
 			int age = scanner.nextInt();
 			System.out.println("部署：");
 			String section = scanner.next();
-			createInsertSQL(tableName, code, name, age, section, stmt);
+			executeInsertSQL(tableName, code, name, age, section, stmt);
 		}
 	}
 
 	// INSERTのSQL文を生成するメソッド
-	public static void createInsertSQL(String tableName, int code, String name, int age, String section, Statement stmt)
-			throws SQLException {
-		stmt.executeUpdate("insert into " + tableName + " (code, name, age, section) values ('" + code + "', '" + name
-				+ "', " + age + ", '" + section + "')");
+	public static void executeInsertSQL(String tableName, int code, String name, int age, String section,
+			Statement stmt) throws SQLException {
+			stmt.executeUpdate(String.format("insert into %s (code, name, age, section) values (%d, %s, %d, %s)", code, name, age, section));
 	}
 
 	// 社員情報を削除するメソッド
-	public static void deleteDB(Statement stmt, String tableName) throws SQLException {
+	public static void deleteEmployee(Statement stmt, String tableName) throws SQLException {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("社員情報を削除しますか？（1:削除する）：");
 		if (scanner.nextInt() == 1) {
 			System.out.print("データを削除する社員の社員番号を入力してください：");
 			int deleteCode = scanner.nextInt();
-			createDeleteSQL(tableName, deleteCode, stmt);
+			executeDeleteSQL(tableName, deleteCode, stmt);
 		}
 	}
 
 	// DELETEのSQL文を生成するメソッド
-	public static void createDeleteSQL(String tableName, int deleteCode, Statement stmt) throws SQLException {
-		stmt.executeUpdate("delete from " + tableName + " where code = " + deleteCode);
+	public static void executeDeleteSQL(String tableName, int deleteCode, Statement stmt) throws SQLException {
+		stmt.executeUpdate(String.format("delete from %s where code = %d", tableName, deleteCode));
+		
 	}
 
 	// 社員情報を更新するメソッド
-	public static void updateDB(Statement stmt, String tableName) throws SQLException {
+	public static void updateEmployee(Statement stmt, String tableName) throws SQLException {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("社員情報を変更しますか？（1:変更する）：");
 		if (scanner.nextInt() == 1) {
@@ -97,25 +97,25 @@ public class SelectEmployees {
 					int initialCode = updateCode;
 					updateCode = scanner.nextInt();
 					changeDataLabel = "code";
-					createUpdateSQL(tableName, updateCode, stmt, changeDataLabel, initialCode);
+					executeUpdateSQL(tableName, updateCode, stmt, changeDataLabel, initialCode);
 					break;
 				case 2:
 					System.out.print("変更後の名前を入力してください：");
 					String updatedName = scanner.next();
 					changeDataLabel = "name";
-					createUpdateSQL(tableName, updatedName, stmt, changeDataLabel, updateCode);
+					executeUpdateSQL(tableName, updatedName, stmt, changeDataLabel, updateCode);
 					break;
 				case 3:
 					System.out.print("変更後の年齢を入力してください：");
 					int updatedAge = scanner.nextInt();
 					changeDataLabel = "age";
-					createUpdateSQL(tableName, updatedAge, stmt, changeDataLabel, updateCode);
+					executeUpdateSQL(tableName, updatedAge, stmt, changeDataLabel, updateCode);
 					break;
 				case 4:
 					System.out.print("変更後の部署を入力してください：");
 					String updatedSection = scanner.next();
 					changeDataLabel = "section";
-					createUpdateSQL(tableName, updatedSection, stmt, changeDataLabel, updateCode);
+					executeUpdateSQL(tableName, updatedSection, stmt, changeDataLabel, updateCode);
 					break;
 				}
 				System.out.print("データの変更を終了しますか？（1:終了）：");
@@ -127,21 +127,19 @@ public class SelectEmployees {
 	}
 
 	// UPDATEのSQL文を生成するメソッド（int型のデータを変更）
-	public static void createUpdateSQL(String tableName, int updatedData, Statement stmt, String changeDataLabel,
+	public static void executeUpdateSQL(String tableName, int updatedData, Statement stmt, String changeDataLabel,
 			int updateEmployeeCode) throws SQLException {
-		stmt.executeUpdate("update " + tableName + " set " + changeDataLabel + " = " + updatedData + " where code = "
-				+ updateEmployeeCode);
+			stmt.executeUpdate(String.format("update %s set %s = %d where code = %d", tableName, changeDataLabel, updatedData, updateEmployeeCode));
 	}
 
 	// UPDATEのSQL文を生成するメソッド（String型のデータを変更）
-	public static void createUpdateSQL(String tableName, String updatedData, Statement stmt, String changeDataLabel,
+	public static void executeUpdateSQL(String tableName, String updatedData, Statement stmt, String changeDataLabel,
 			int updateEmployeeCode) throws SQLException {
-		stmt.executeUpdate("update " + tableName + " set " + changeDataLabel + " = " + updatedData + " where code = "
-				+ updateEmployeeCode);
+			stmt.executeUpdate(String.format("update %s set %s = %s where code = %d", tableName, changeDataLabel, updatedData, updateEmployeeCode));
 	}
 
 	// SELECTのSQL文を生成するメソッド
-	public static ResultSet createSelectSQL(Statement stmt, String tableName) throws SQLException {
-		return stmt.executeQuery("select * from " + tableName);
+	public static ResultSet executeSelectSQL(Statement stmt, String tableName) throws SQLException {
+		return stmt.executeQuery(String.format("select * from %s", tableName));
 	}
 }
